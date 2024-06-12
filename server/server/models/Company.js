96212f2d4b087767cc3d262d03f2f6dad325c281
Halwest Abubakir <halwest.east@gmail.com>
2024-06-12 22:44:07 +0300

    Feat: add Company Model Schema Fixes #6

--- server/server/models/Company.js
+++ server/server/models/Company.js
@@ -3,35 +3,67 @@ const mongoose = require("mongoose");
 const companySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please provide a name for the company"],
+    required: [true, "Please add company name"],
   },
   website: {
     type: String,
-    required: false,
   },
-  descrition: {
+  description: {
     type: String,
-    required: [true, "Please add company description"],
   },
   logo: {
     type: String,
-    required: false,
   },
   location: {
     type: String,
-    required: [true, "Please set company location"],
   },
   CEO: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "CEO",
+    name: {
+      type: String,
+      required: [true, "Please add CEO name"],
+    },
+    bio: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
   },
-  contactInfo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "ContactInfo",
+  ContactInfo: {
+    address: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
   },
-  history: { type: String, required: false },
-  mission: { type: String, required: false },
-  history: { type: [String], required: false },
+  history: {
+    type: String,
+  },
+  mission: {
+    type: String,
+  },
+  values: {
+    type: [String],
+  },
 });
 
 module.exports = mongoose.model("Company", companySchema);
