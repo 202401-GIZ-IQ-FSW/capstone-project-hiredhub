75ad0cb3bc20b5b096c4c4c587adabdcd6ed78f0
schinak-m <shinak.x3@gmail.com>
2024-06-15 15:03:07 +0300

    update virtul field to get full name and include it in personal info

--- server/server/models/Profile.js
+++ server/server/models/Profile.js
@@ -20,11 +20,25 @@ const profileSchema = new Schema(
     applications: [{ type: mongoose.Schema.Types.ObjectId, ref: "Application"}],
     savedJobs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Job"}],
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
-profileSchema.virtual('fullName').get(function() {
-    return `${this.personalInfo.firstName} ${this.personalInfo.lastName}`;
+profileSchema.virtual('personalInfo.fullName').get(function() {
+  return `${this.personalInfo.firstName} ${this.personalInfo.lastName}`;
+});
+
+profileSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, ret) => {
+    if (ret.personalInfo) {
+      ret.personalInfo.fullName = `${ret.personalInfo.firstName} ${ret.personalInfo.lastName}`;
+    }
+    return ret;
+  }
 });
 
 module.exports = mongoose.model("Profile", profileSchema);
