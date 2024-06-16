72cd1504597d66c3ce330afcbf1c041dce99e2b4
Halwest Abubakir <halwest.east@gmail.com>
2024-06-16 23:32:14 +0300

    Feat: add rate limiter middleware

---
+++ server/server/middlewares/rateLimiter.js
@@ -0,0 +1,15 @@
+//this middleware can be used globally by importing it to the server.js
+
+const rateLimit = require("express-rate-limit");
+
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    status: 429,
+    message: "Too many requests, please try again later.",
+  },
+  headers: true,
+});
+
+module.exports = rateLimiter;
