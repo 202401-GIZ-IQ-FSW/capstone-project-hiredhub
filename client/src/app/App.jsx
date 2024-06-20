f9487a62c330cae18e944cd09e83d6c33f37975c
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-06-21 00:17:02 +0300

    SearchBox component 39

--- client/src/app/App.jsx
+++ client/src/app/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "@/components/NavBar/NavBar";
+import SearchBox from "@/components/SearchBox/SearchBox";
 
 const App = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl items-center justify-between font-mono text-sm lg:flex w-[85%] m-auto bg-white">
         <NavBar />
+        <SearchBox />
       </div>
     </main>
   );
---
+++ client/src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,26 @@
+import { Butterfly_Kids } from "next/font/google";
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+
+const SearchBox = () => {
+  return (
+    <div className="searchBox grid gp-10 bg-[#CAF4FF] rounded-[10px] p-[3rem]">
+      <form action="">
+        <div className=" flex justify-between items-center rounded-[8px] gap-[10px] bg-white p-5 shadow-lg shadow-gray-400">
+          <div className="flex gap-2 items-center">
+            <IoSearch className="searchIcon text-[25px] cursor-pointer" />
+            <input
+              type="text"
+              className="bg-transparent text-blue-500 focus:outline-none w-[100%]"
+              placeholder="Browse Jobs ..."
+            />
+          </div>
+          <button className="searchButton bg-[#3081D0] h-full p-5 px-10 rounded-[10px] text-white hover:bg-[#A0DEFF] hover:text-gray-700">
+            Find Job!
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+export default SearchBox;
