2d834cb7475228a2a38a2efb3442fb693d9c8a2c
Mohammed Nazar <x3raqe@gmail.com>
2024-06-21 22:28:32 +0300

    get rid of db live url

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -7,9 +7,6 @@ const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
   process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
 }:${DB_PORT}/${DB_NAME}?authSource=admin`;
 
-// lIVE DB URI FOR MOHAMMED NAZAR FELL FREE TO USE IT
-// COMMENT THE ABOVE DB_URI AND UNCOMMENT THE BELOW LINE
-// const DB_URI = `mongodb+srv://x3raqe:1234@test1.qll8xfz.mongodb.net/?retryWrites=true&w=majority&appName=Test1`;
 
 
 const url = DB_URI;
