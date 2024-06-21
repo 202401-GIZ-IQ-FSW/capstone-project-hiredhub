8d741986d64094b4d5103653e74e3e72dfda51ef
Mohammed Nazar <x3raqe@gmail.com>
2024-06-21 19:08:44 +0300

    Update job schema

--- server/server/models/Job.js
+++ server/server/models/Job.js
@@ -1,80 +1,84 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const jobSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  field: {
-    type: String,
-    enum: ['Development','Engineering', 'Marketing', 'Finance', 'Healthcare', 'Education', 'Other'], 
-    required: true
-  },
-  category: {
-    type: String,
-    enum: ['Full-Time', 'Part-Time', 'Contract', 'Internship', 'Temporary', 'Freelance'], 
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  position: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  company: {
-    type: Schema.Types.ObjectId,
-    ref: 'Company',
-    required: true
-  },
-  employment: {
-    type: String,
-    required: true
-  },
-  wage: {
+const jobSchema = new Schema(
+  {
+    companyId: {
+      type: Schema.Types.ObjectId,
+      ref: "Company",
+      required: true,
+    },
+    wage: {
     type: Number,
     required: true
+          },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    jobType: {
+        type: [String],
+        enum: ['Full-Time', 'Part-Time', 'Contract', 'Internship', 'Temporary', 'Freelance', 'On-site', 'Hybrid', 'Remote'], 
+        required: true
+    },
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    location: {
+      type: String,
+      required: true,
+    },
+    requirements: {
+      educationalLevel: {
+        type: String,
+        enum: ['High School', 'Associate', 'Bachelor', 'Master', 'Doctorate'],
+      },
+      yearsOfExperience: {
+        type: Number,
+        enum: ['0-1 years', '1-3 years', '3-5 years', '5-7 years', '7+ years'] 
+      },
+      certifications: [
+        {
+          type: String,
+        },
+      ],
+      skills: [
+        {
+          type: String,
+        },
+      ],
+    },
+    applicationDetails: {
+      deadline: {
+        type: Date,
+        required: true,
+      },
+      method: {
+        type: String,
+      },
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    applicants: {
+        type: [String]
+    },
+    status: {
+      type: String,
+      enum: ["open", "closed", "paused"],
+      default: "open",
+    },
   },
-  minEduLevel: {
-    type: String,
-    enum: ['High School', 'Associate', 'Bachelor', 'Master', 'Doctorate'],
-    required: true
-  },
-  yearExperience: {
-    type: String,
-    enum: ['0-1 years', '1-3 years', '3-5 years', '5-7 years', '7+ years'], 
-    required: true
-  },
-  certifications: {
-    type: [String]
-  },
-  skills: {
-    type: [String],
-    required: true
-  },
-  appMethod: {
-    type: String,
-    enum: ['Online', 'Email', 'In-Person'],
-    required: true
-  },
-  appDeadline: {
-    type: Date,
-    required: true
-  },
-  views: {
-    type: Number,
-    default: 0
-  },
-  applicants: {
-    type: [String]
-  }
-});
+  {
+    timestamps: true,
+  })
 
 
-module.exports = mongoose.model("Job", jobSchema);
+module.exports = mongoose.model("Job", jobSchema); 
