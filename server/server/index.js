6a33023ea7cae45a360790623129760f6047103f
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-06-28 19:18:18 +0300

    adding all getter file  and removing the xtra router

--- server/server/index.js
+++ server/server/index.js
@@ -1,26 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const companyRoutes = require("./routes/companyRoutes");
-const uploadRoutes = require("./routes/uploadRoute")
-
-
+const getFile = require("./routes/RetriveFile");
 require("dotenv").config();
-
 
 //DB Connect
 const connectToMongo = require("./db/connection");
 const logging = require("./middlewares/logging");
 
 //Route Imports
-const companyRoutes = require("./routes/companyRoutes");
+// const companyRoutes = require("./routes/companyRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
 const authRoutes = require("./routes/authRoutes");
 const emailRoutes = require("./routes/emailRoutes");
-const profileRoutes = require('./routes/profileRoutes')
-
-
-
-
+const profileRoutes = require("./routes/profileRoutes");
 const app = express();
 const port =
   process.env.NODE_ENV === "test"
@@ -31,30 +24,22 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-
-
 // logging middleware
 app.use(logging());
 
-
 //ROUTES
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use("/api/companies", companyRoutes);
-app.use("/api/send-email", emailRoutes)
-app.use('/api/profile', profileRoutes)
+app.use("/api/send-email", emailRoutes);
+app.use("/api/profile", profileRoutes);
 app.use("/api/", categoryRoutes);
-
-
+app.use("/backend-app", getFile);
 //SERVER
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
   connectToMongo();
 });
-
-app.use("api/upload", uploadRoutes)
-
 
 app.get("/test", (req, res) => {
   res.json(
