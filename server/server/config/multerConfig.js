f7c16cb22e1792157bc168deef092ba5304da8fc
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-06-28 19:16:47 +0300

    modifing the multer to adjust to the system and adding the getter of teh files

--- server/server/config/multerConfig.js
+++ server/server/config/multerConfig.js
@@ -1,26 +1,37 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
+const uploadDir = path.join(__dirname, "..", "files");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, uploadDir);
   },
+
   filename: function (req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  }
+    const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+    const extension = path.extname(file.originalname);
+    cb(null, `${uniqueSuffix}${extension}`);
+  },
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'application/pdf') {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "application/pdf"
+  ) {
     cb(null, true);
   } else {
-    cb(new Error('Invalid file type, only JPEG, PNG, and PDF is allowed!'), false);
+    cb(
+      new Error("Invalid file type, only JPEG, PNG, and PDF is allowed!"),
+      false
+    );
   }
 };
 
 const upload = multer({
   storage: storage,
-  fileFilter: fileFilter
+  fileFilter: fileFilter,
 });
 
 module.exports = upload;
--- server/server/controllers/uploadController/uploadController.js
+++ server/server/controllers/uploadController/uploadController.js
@@ -1,43 +1,23 @@
-const multer = require('multer');
-const Profile = require('../../models/Profile');
+const path = require("path");
+const fs = require("fs");
 
-const upload = multer({
-  dest: 'uploads/', 
-  limits: { fileSize: 1000000 }, 
-});
+const uploadDir = path.join(__dirname, "..", "..", "files");
 
-exports.uploadProfilePicture = upload.single('profilePicture'), async (req, res) => {
-  try {
-    const profile = await Profile.findById(req.user.id); 
+exports.getFile = (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(uploadDir, filename);
 
-    if (!profile) {
-      return res.status(404).json({ message: 'User profile not found' });
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.error("File not found:", err);
+      return res.status(404).send("File not found");
     }
 
-    profile.personalInfo.profilePicture = req.file.filename;
-    await profile.save();
-
-    res.json({ message: 'Profile picture uploaded successfully' });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: 'Server Error' });
-  }
-};
-
-exports.uploadCV = upload.single('cv'), async (req, res) => {
-  try {
-    const profile = await Profile.findById(req.user.id); 
-
-    if (!profile) {
-      return res.status(404).json({ message: 'User profile not found' });
-    }
-
-    profile.personalInfo.resume = req.file.filename;
-    await profile.save();
-
-    res.json({ message: 'CV uploaded successfully' });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: 'Server Error' });
-  }
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        console.error("Error sending file:", err);
+        return res.status(500).send("Error sending file");
+      }
+    });
+  });
 };
