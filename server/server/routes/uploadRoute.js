3a3aa490bc6ef7583fd5ff489812ca3c9d359d1e
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-06-28 19:15:43 +0300

    removing extra file

--- server/server/routes/uploadRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const profileController = require("../controllers/uploadController/uploadController");
-
-const router = express.Router();
-
-router.post('/upload/profilepicture', profileController.uploadProfilePicture);
-router.post('/upload/cv', profileController.uploadCV);
-
-module.exports = router;
