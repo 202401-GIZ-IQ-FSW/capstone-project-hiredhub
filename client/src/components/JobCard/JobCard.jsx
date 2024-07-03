b9a3ad960b69ed534f5fa1c6bda6fc1b94a5c0f2
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-07-03 21:13:52 +0300

    redesigned

--- client/src/components/JobCard/JobCard.jsx
+++ client/src/components/JobCard/JobCard.jsx
@@ -2,19 +2,21 @@ import React from "react";
 
 const JobCard = () => {
   return (
-    <div className="job-card max-w-sm bg-white rounded-lg shadow-md hover:shadow-xl hover:bg-blue-100 overflow-hidden m-4">
-      <div className="p-4">
-        <h2 className="text-xl font-semibold text-gray-800">title</h2>
-        <p className="text-gray-600 mt-2">Employer Company</p>
-        <p className="text-gray-500 text-sm mt-1">City/Location</p>
-        <p className="text-gray-700 mt-4">
+    <div class="job-card max-w-2xl bg-[#243238] rounded-lg shadow-md hover:shadow-xl hover:bg-slate-600 overflow-hidden m-4 flex flex-col md:flex-row">
+      <div class="p-4 flex-grow">
+        <h2 class="text-xl font-semibold text-white">Job Title</h2>
+        <p class="text-white mt-2">Employer Company</p>
+        <p class="text-[#9ea5a8] text-sm mt-1">City/Location</p>
+        <p class="text-[#9ea5a8] mt-4">
           DESCRIPTION Lorem ipsum dolor sit amet consectetur, adipisicing elit.
           Tempora iste veniam nesciunt, cum doloribus laboriosam voluptates ipsa
           sunt, expedita iure, et saepe ex repellat itaque natus eius suscipit.
           Accusantium, dolore.
         </p>
-        <button className="mt-6 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600">
-          Apply Now
+      </div>
+      <div class="p-4 flex md:items-end items-center justify-center">
+        <button class="bg-[#fafdfc] text-[#243238] py-2 px-4 rounded-lg hover:bg-[#243238] hover:text-white">
+          Apply
         </button>
       </div>
     </div>
