83640371e18c16a895b2a1d9176a6e97faca4f8f
Halwest Abubakir <halwest.east@gmail.com>
2024-07-04 11:23:28 +0300

    feat: edit responsiveness

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -6,22 +6,22 @@ import Image from "next/image";
 function Hero() {
   return (
     <div>
-      <div className="grid xl:grid-cols-2 sm:grid-cols-1 mx-9 mt-9 justify-items-center ">
-        <div className="flex flex-col justify-center mx-12 gap-10">
+      <div className="grid xl:grid-cols-2 sm:grid-cols-1 mx-9 mt-9  justify-items-center ">
+        <div className="flex flex-col justify-center xl:mx-12 sm:mx-4 gap-10 xl:mb-4 sm:mb-7">
           <div className="flex flex-col gap-5">
-            <h1 className="text-5xl font-mulish font-semibold xl:text-left sm:text-center ">
+            <h1 className="font-mulish xl:text-5xl sm:text-4xl font-semibold xl:text-left sm:text-center ">
               Let the jobs Find you
             </h1>
-            <h3 className="text-2xl font-lato xl:w-full xl:text-left sm:text-center sm:w-[500px]">
+            <h3 className="xl:text-2xl sm:text-xl font-lato xl:w-full xl:text-left sm:text-center sm:w-[500px]">
               Create your free profile to get interview invites and jobs that
               work for you.
             </h3>
           </div>
           <div className="flex flex-col gap-5">
-            <h2 className="text-4xl font-mulish font-semibold xl:text-left sm:text-center">
+            <h2 className="xl:text-4xl sm:text-3xl font-mulish font-semibold xl:text-left sm:text-center">
               Skip the paperwork
             </h2>
-            <h4 className="text-xl font-lato">
+            <h4 className="xl:text-2xl sm:text-xl font-lato">
               Your profile is your application. Apply to jobs instantly.
             </h4>
           </div>
@@ -37,9 +37,7 @@ function Hero() {
       </div>
 
       <div className="flex flex-col  w-screen place-items-center place-content-center mt-10">
-        <h1 className="text-2xl mb-4 font-raleway">
-          Let's find your next job
-        </h1>
+        <h1 className="text-2xl mb-4 font-raleway">Let's find your next job</h1>
         <div className="flex  items-center space-x-2 mb-4">
           <Input
             className="xl:w-[500px] sm:w-[300px]"
