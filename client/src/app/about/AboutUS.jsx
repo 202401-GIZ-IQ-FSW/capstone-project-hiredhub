20e4c5932624d6845710846960caf9605d62c807
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-07-05 02:04:09 +0300

    Team Members section

--- client/src/app/about/AboutUS.jsx
+++ client/src/app/about/AboutUS.jsx
@@ -37,6 +37,85 @@ const AboutUS = () => {
         </div>
       </div>
     </div>
+    <div className="my-8">
+      <h2 className="text-2xl text-[#40A578] font-semibold mb-8">
+        Meet Our Team
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <img
+            src=""
+            alt="Halwest "
+            className="w-32 h-32 rounded-lg border-[#40A578] border-2 mx-auto"
+          />
+          <h3 className="text-xl font-semibold mt-4 text-center">Halwest</h3>
+          <p className="text-gray-600 text-center">Job Title</p>
+          <p className="text-gray-700 mt-2 text-center">
+            "Job description briefly"
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <img
+            src=""
+            alt="Schinak"
+            className="w-32 h-32 rounded-lg border-[#40A578] border-2  mx-auto"
+          />
+          <h3 className="text-xl font-semibold mt-4 text-center">
+            Schinak Mohamed
+          </h3>
+          <p className="text-gray-600 text-center">Job Title</p>
+          <p className="text-gray-700 mt-2 text-center">
+            "Job description briefly"
+          </p>
+        </div>
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <img
+            src=""
+            alt="Mo Nazar"
+            className="w-32 h-32 rounded-lg border-[#40A578] border-2  mx-auto"
+          />
+          <h3 className="text-xl font-semibold mt-4 text-center">
+            Mohammad Nazar
+          </h3>
+          <p className="text-gray-600 text-center">Job Title</p>
+          <p className="text-gray-700 mt-2 text-center">
+            "Job description briefly"
+          </p>
+        </div>
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <img
+            src=""
+            alt="Hana"
+            className="w-32 h-32 rounded-lg border-[#40A578] border-2  mx-auto"
+          />
+          <h3 className="text-xl font-semibold mt-4 text-center">Hana</h3>
+          <p className="text-gray-600 text-center">Job Title</p>
+          <p className="text-gray-700 mt-2 text-center">
+            "Job description briefly"
+          </p>
+        </div>
+
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <img
+            src=""
+            alt="Dilan nophoto"
+            className="w-32 h-32 rounded-lg border-[#40A578] border-2  mx-auto"
+          />
+          <h3 className="text-xl font-semibold mt-4 text-center">
+            Dilan Ahmed
+          </h3>
+          <p className="text-gray-600 text-center">
+            Business Developer/Consultant
+          </p>
+          <p className="text-gray-700 mt-2 text-center">
+            Dilan improves the business aspects of our company and provides
+            consultancy to the employers and the cnadidates to ensure securing
+            their desired end results.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>;
 };
 export default AboutUS;
