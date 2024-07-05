b162ecd4e1026ba0fa2b4edabfc545a8b4fe535c
Dilan Ahmed <dilan98mustafa@gmail.com>
2024-07-05 11:55:31 +0300

    slightchange

--- client/src/components/SearchBox/SearchBox.jsx
+++ client/src/components/SearchBox/SearchBox.jsx
@@ -4,18 +4,18 @@ import { IoSearch } from "react-icons/io5";
 
 const SearchBox = () => {
   return (
-    <div class="searchBox grid rounded-lg p-6 md:p-12 ">
+    <div className="searchBox grid rounded-lg p-6 md:p-12 ">
       <form action="">
-        <div class="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 rounded-lg bg-white p-4 md:p-6 shadow-lg">
-          <div class="flex items-center w-full md:w-auto gap-2">
-            <IoSearch class="searchIcon cursor-pointer text-2xl md:text-3xl" />
+        <div className="flex flex-col md:flex-row items-center justify-between gap-4 md:gap-6 rounded-lg bg-white p-4 md:p-6 shadow-lg">
+          <div className="flex items-center w-full md:w-auto gap-2">
+            <IoSearch className="searchIcon cursor-pointer text-2xl md:text-3xl" />
             <input
               type="text"
-              class="w-full md:w-auto flex-grow bg-transparent text-green-700 focus:outline-none text-lg md:text-xl"
+              className="w-full md:w-auto flex-grow bg-transparent text-green-700 focus:outline-none text-lg md:text-xl"
               placeholder="Browse Jobs ..."
             />
           </div>
-          <button class="w-full md:w-auto rounded-lg bg-white py-2 md:py-3 px-6 md:px-10 text-gray-900 hover:bg-green-700 hover:text-gray-100 font-bold">
+          <button className="w-full md:w-auto rounded-lg bg-white py-2 md:py-3 px-6 md:px-10 text-gray-900 hover:bg-green-700 hover:text-gray-100 font-bold">
             Find Job!
           </button>
         </div>
