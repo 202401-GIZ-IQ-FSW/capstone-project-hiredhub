1e57de9cb2b1bb2702d3f4f21da84fb8a3242b69
Halwest Abubakir <halwest.east@gmail.com>
2024-07-06 09:56:28 +0300

    Feat: add mobile view

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -7,26 +7,26 @@ function Hero() {
   return (
     <div>
       <div className="grid xl:grid-cols-2 sm:grid-cols-1 mx-9 mt-9  justify-items-center ">
-        <div className="flex flex-col justify-center xl:mx-12 sm:mx-4 gap-10 xl:mb-4 sm:mb-7">
+        <div className="flex flex-col justify-center xl:mx-12 sm:mx-4 gap-10 mb-10 xl:mb-4 sm:mb-7">
           <div className="flex flex-col gap-5">
-            <h1 className="font-mulish xl:text-5xl sm:text-4xl font-semibold xl:text-left sm:text-center ">
+            <h1 className="font-mulish text-3xl text-center xl:text-5xl sm:text-4xl font-semibold xl:text-left sm:text-center ">
               Let the jobs Find you
             </h1>
-            <h3 className="xl:text-2xl sm:text-xl font-lato xl:w-full xl:text-left sm:text-center sm:w-[500px]">
+            <h3 className="xl:text-2xl sm:text-xl text-center font-lato xl:w-full xl:text-left sm:text-center sm:w-[500px]">
               Create your free profile to get interview invites and jobs that
               work for you.
             </h3>
           </div>
           <div className="flex flex-col gap-5">
-            <h2 className="xl:text-4xl sm:text-3xl font-mulish font-semibold xl:text-left sm:text-center">
+            <h2 className="text-2xl text-center xl:text-4xl sm:text-3xl font-mulish font-semibold xl:text-left sm:text-center">
               Skip the paperwork
             </h2>
-            <h4 className="xl:text-2xl sm:text-xl font-lato">
+            <h4 className="xl:text-2xl sm:text-xl md:text-left text-center font-lato">
               Your profile is your application. Apply to jobs instantly.
             </h4>
           </div>
         </div>
-        <div className="sm:p-10 xl:p-1">
+        <div className="sm:p-10 xl:p-1 w-[300px] xl:w-[450px] md:w-[450px] sm:w-[400]">
           <Image
             src={hero}
             width={500}
