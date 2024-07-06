286783ab5fbf48393a18e1b3a392fb1a8bf5ca79
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-07 00:16:09 +0300

    newsest update

--- client/src/components/JobInfo/JobInfo.jsx
+++ client/src/components/JobInfo/JobInfo.jsx
@@ -1,23 +1,6 @@
-"use client";
-import {
-  Description,
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  DialogTrigger,
-  DialogHeader,
-  DialogFooter,
-} from "@radix-ui/react-dialog";
-import { useState } from "react";
 import { Button } from "../ui/button";
 
 function JobInfo({ job }) {
-  const [jobInfo, setJobInfo] = useState(false);
-  const handlerDialog = () => {
-    setJobInfo(!jobInfo);
-    console.log(jobInfo);
-  };
-
   return (
     <div className=' max-w-lg'>
       <h1 className='text-start text-xl'>{job.title}</h1>
@@ -27,26 +10,9 @@ function JobInfo({ job }) {
         <p>Job description : {job.description}</p>
         <p> Job Qualification : {job.qualification}</p>
         <p>Job Expirence : {job.expirence}</p>
-        <p></p>
       </div>
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button onClick={() => handlerDialog}> Apply </Button>
-        </DialogTrigger>
-        <DialogContent className='sm:max-w-[425px]'>
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <Description>
-              Are you sure , you applie with the current Cv{" "}
-            </Description>
-          </DialogHeader>
-
-          <DialogFooter>
-            <Button type='submit'>Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+      <Button> Apply </Button>
     </div>
   );
 }
