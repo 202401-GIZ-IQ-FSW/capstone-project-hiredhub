44a87e2d78ea04a07b15d37589c8f39982eeb2ba
schinak-m <shinak.x3@gmail.com>
2024-07-12 00:50:08 +0300

    build job posting form

--- server/server/models/Category.js
+++ server/server/models/Category.js
@@ -1,8 +1,38 @@
 const mongoose = require("mongoose");
 
+const categoryEnum = [
+  "Software Development",
+  "Web Development",
+  "Data Science",
+  "Accounting",
+  "Finance",
+  "Administrative",
+  "Architecture",
+  "Engineering",
+  "Art & Design",
+  "Customer Service",
+  "Education",
+  "Healthcare",
+  "Human Resources",
+  "Legal",
+  "Logistics",
+  "Marketing",
+  "Media",
+  "Non-Profit",
+  "Operations Management",
+  "Project Management",
+  "Research",
+  "Retail",
+  "Sales",
+  "Security",
+  "Social Services",
+  "Writing"
+];
+
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
+    enum: categoryEnum,
     required: true,
     unique: true,
   },
@@ -13,4 +43,3 @@ const categorySchema = new mongoose.Schema({
 
 const Category = mongoose.model("Category", categorySchema);
 module.exports = Category;
-
