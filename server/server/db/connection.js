4b1130e011da3fef68864e6afab2c2214408f457
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-11 20:05:27 +0300

    change db and not using docker container of db

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
   process.env;
 
-const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
+const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${
   process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
-}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+}/?retryWrites=true&w=majority&appName=${DB_NAME}`;
+
 mongoose.set("strictQuery", false);
 
 const url = DB_URI;
