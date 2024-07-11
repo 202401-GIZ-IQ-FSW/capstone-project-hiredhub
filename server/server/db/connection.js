ff5a7700aeb60e955097d205a786987a2bc5802a
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-11 21:26:01 +0300

    change db

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -5,7 +5,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
 
 const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${
   process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
-}/?retryWrites=true&w=majority&appName=${DB_NAME}`;
+}/${DB_NAME}?retryWrites=true&w=majority&appName=${DB_NAME}`;
 
 mongoose.set("strictQuery", false);
 
