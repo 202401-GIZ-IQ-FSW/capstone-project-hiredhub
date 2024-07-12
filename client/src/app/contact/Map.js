16c847ea0758f5e6d093cb3907349c2587a22cae
Mohammed Nazar <x3raqe@gmail.com>
2024-07-12 21:51:43 +0300

    done with contact page

---
+++ client/src/app/contact/Map.js
@@ -0,0 +1,16 @@
+const Map = () => {
+    return (
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1654479.4175978939!2d43.80523675!3d35.91258285!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40090675168d57f5%3A0x18d1cbbe61767ce8!2sKurdistan%20Region!5e0!3m2!1sen!2siq!4v1720806217577!5m2!1sen!2siq"
+        height="300"
+        className="md:w-9/12 md:h-5/6"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+    );
+  }
+  
+  export default Map;
+  
