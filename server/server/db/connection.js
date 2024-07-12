5cf676aba50adfff2bf2e2f400c36e539be3bc63
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-12 17:06:36 +0300

    update db

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -3,11 +3,13 @@ const mongoose = require("mongoose");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
   process.env;
 
-const DB_URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${
-  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
-}/${DB_NAME}?authSource=admin`;
-mongoose.set("strictQuery", false);
+  const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${
+    process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
+  }/${DB_NAME}?retryWrites=true&w=majority&appName=${DB_NAME}`;
 
+
+mongoose.set("strictQuery", false);
+console.log("DB_HOST", DB_HOST);
 const url = DB_URI;
 console.log("url################################", url);
 const connectToMongo = () => {
