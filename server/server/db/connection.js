bff76c1b2e3b24791a5abb3946ac43f1a3f35789
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-12 18:33:10 +0300

    chaning the db

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -1,17 +1,10 @@
 const mongoose = require("mongoose");
 
-const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME,
-  TEST_DB_HOST,
-  MONGODB_DATABASE,
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
+  process.env;
 
 const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${
-  process.env.NODE_ENV === "test" ? TEST_DB_HOST : MONGODB_DATABASE
+  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
 }/?retryWrites=true&w=majority&appName=${DB_NAME}`;
 
 mongoose.set("strictQuery", false);
