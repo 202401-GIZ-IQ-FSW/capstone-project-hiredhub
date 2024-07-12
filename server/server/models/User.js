4b4a2f8fdafcf04e257182de2e084246eca602aa
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-12 14:56:55 +0300

    loging the db and fixing the user unique google id null_value

--- server/server/models/User.js
+++ server/server/models/User.js
@@ -1,23 +1,27 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  googleId: {
-    type: String,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      unique: true,
+      sparse: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["jobSeeker", "employer"],
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String
-  },
-  role: {
-    type: String,
-    enum: ["jobSeeker", "employer"],
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
