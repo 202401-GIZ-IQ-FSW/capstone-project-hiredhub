ba9a358e357a5dc6575738ac5f91df3e2cbf8645
hanaMohammedAbdullah <hana.muhammed.abdulla@gmail.com>
2024-07-12 17:28:17 +0300

    change the db

--- server/server/db/connection.js
+++ server/server/db/connection.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, TEST_DB_HOST } =
   process.env;
-const password = encodeURIComponent(DB_PASSWORD);
-const DB_URI = `mongodb+srv://${DB_USER}:${password}@${
-  process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
-}/?retryWrites=true&w=majority&appName=${DB_NAME}&authSource=admin`;
+  const DB_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${
+    process.env.NODE_ENV === "test" ? TEST_DB_HOST : DB_HOST
+  }/?retryWrites=true&w=majority&appName=${DB_NAME}`;
 
 mongoose.set("strictQuery", false);
 console.log("DB_HOST", DB_HOST);
