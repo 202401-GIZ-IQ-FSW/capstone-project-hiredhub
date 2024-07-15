9f26ff8d4c2faa1305f66f652eb14b47dd9c2021
Mohammed Nazar <x3raqe@gmail.com>
2024-07-15 15:44:04 +0300

    Revert "use client for company page"
    This reverts commit ea41fb6c061543316850bb23bfb7c2b4682e3b1b.

--- client/src/app/company/[id]/page.js
+++ client/src/app/company/[id]/page.js
@@ -1,32 +1,42 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Image from "next/image";
-import { FaHouse } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa6";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaPhoneAlt } from "react-icons/fa";
+  FaHouse,
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+  FaXTwitter,
+  FaPhoneAlt,
+} from "react-icons/fa";
+
 import { MdOutlineMailOutline } from "react-icons/md";
+import { useState, useEffect } from "react";
 
 const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-const getCompanyById = async (companyId) => {
-  const response = await fetch(`${backendURL}/companies/${companyId}`, {
-    method: 'GET'
-  })
 
-  return response.json()
-}
+const page = ({ params }) => {
+  const [company, setCompany] = useState("");
 
-const page = async ({params}) => {
-  const company  = await getCompanyById(params.id)
+  const getCompanyById = async (companyId) => {
+    const response = await fetch(`${backendURL}/companies/${companyId}`, {
+      method: "GET",
+    });
+
+    const data = await response.json();
+    console.log(data);
+    return setCompany(data);
+  };
+
+  useEffect(() => {
+    if (params.id) {
+      getCompanyById(params.id);
+    }
+  }, []);
+
   return (
     <div>
       <div className="flex min-h-screen w-full flex-col">
@@ -35,18 +45,35 @@ const page = async ({params}) => {
             <Card x-chunk="dashboard-01-chunk-0" className="col-span-4">
               <CardHeader className="flex flex-row items-center py-20 justify-between space-y-0 pb-2 bg-gradient-to-r from-[#40A578]  to-[#006769]">
                 <CardTitle className="flex text-sm font-medium  -mb-10 w-20 h-20">
-                    <div className="flex justify-center items-center w-full">
-                        <img src={"/logo-tech.png"} className="" alt="logo" />
-                    </div>
+                  <div className="flex justify-center items-center w-full">
+                    {/* {company?.logo !== undefined ? (
+                      <img
+                        src={company?.logo}
+                        alt={company?.name}
+                        width={80}
+                        height={80}
+                      />
+                    ) : (
+                      <img src={"/logo-tech.png"} className='' alt='logo' />
+                    )} */}
+                    <img src={"/logo-tech.png"} className="" alt="logo" />
+                  </div>
                 </CardTitle>
               </CardHeader>
               <CardContent>
-                <div className="text-2xl font-bold pt-16 mb-2">{company.name}</div>
+                <div className="text-2xl font-bold pt-16 mb-2">
+                  {company?.name}
+                </div>
                 <div className="grid">
-                    <p className="text-sm text-muted-foreground">
-                    {company.location} | <Link href={company.website} className="underline"> {company.website}</Link>
+                  <p className="text-sm text-muted-foreground">
+                    {company?.location} |{" "}
+                    {company?.website && (
+                      <Link href={company?.website} className="underline">
+                        {" "}
+                        {company?.website}
+                      </Link>
+                    )}
                   </p>
-                  
                 </div>
               </CardContent>
             </Card>
@@ -60,66 +87,89 @@ const page = async ({params}) => {
               </CardHeader>
               <CardContent>
                 <div>
-                    <p className="text-muted-foreground mb-10">
-                    {company.description}
-                    </p>
+                  <p className="text-muted-foreground mb-10">
+                    {company?.description}
+                  </p>
 
-                    <h1 className="text-lg font-semibold mb-4">History</h1>
-                    <p className="text-muted-foreground mb-10">
-                    {company.history}
-                    </p>
+                  <h1 className="text-lg font-semibold mb-4">History</h1>
+                  <p className="text-muted-foreground mb-10">
+                    {company?.history}
+                  </p>
 
-                    <h1 className="text-lg font-semibold mb-4">Mission</h1>
-                    <p className="text-muted-foreground mb-10">
-                    {company.mission}
-                    </p>
+                  <h1 className="text-lg font-semibold mb-4">Mission</h1>
+                  <p className="text-muted-foreground mb-10">
+                    {company?.mission}
+                  </p>
 
-                    <h1 className="text-lg font-semibold mb-4">Values</h1>
-                    <ul className="text-muted-foreground mb-10 list-disc ml-4">
-                    {company.values.map((value, index) => (
+                  <h1 className="text-lg font-semibold mb-4">Values</h1>
+                  <ul className="text-muted-foreground mb-10 list-disc ml-4">
+                    {company?.values &&
+                      company?.values.map((value, index) => (
                         <li key={index}>{value}</li>
-                    ))}
-                    </ul>
+                      ))}
+                  </ul>
 
-                    <h1 className="text-lg font-semibold mb-4">Contact Information</h1>
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <FaHouse size={20} className="mr-2" />
-                      {company.ContactInfo?.address ? company.ContactInfo.address : ""}
-                    </p>
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <FaPhoneAlt size={20} className="mr-2" />
-                      {company.ContactInfo?.phone}
-                    </p>
+                  <h1 className="text-lg font-semibold mb-4">
+                    Contact Information
+                  </h1>
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    <FaHouse size={20} className="mr-2" />
+                    {company?.ContactInfo?.address}
+                  </p>
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    <FaPhoneAlt size={20} className="mr-2" />
+                    {company?.ContactInfo?.phone}
+                  </p>
 
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <MdOutlineMailOutline size={20} className="mr-2" />
-                      {company.ContactInfo?.email}
-                    </p>
-                    
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <Link href={company.ContactInfo?.facebook ? company.ContactInfo?.facebook : "#"} className="flex">
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    <MdOutlineMailOutline size={20} className="mr-2" />
+                    {company?.ContactInfo?.email}
+                  </p>
+
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    {company?.ContactInfo?.facebook && (
+                      <Link
+                        href={company?.ContactInfo?.facebook}
+                        className="flex"
+                      >
                         <FaFacebook size={20} className="mr-2" />
                         Facebook
                       </Link>
-                    </p>
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <Link href={company.ContactInfo?.instagram ? company.ContactInfo?.instagram : "#"} className="flex">
+                    )}
+                  </p>
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    {company?.ContactInfo?.instagram && (
+                      <Link
+                        href={company?.ContactInfo?.instagram}
+                        className="flex"
+                      >
                         <FaInstagram size={20} className="mr-2" />
                         Instagram
                       </Link>
-                    </p>
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <Link href={company.ContactInfo?.linkedin ? company.ContactInfo?.linkedin : "#"} className="flex">
+                    )}
+                  </p>
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    {company?.ContactInfo?.linkedin && (
+                      <Link
+                        href={company?.ContactInfo?.linkedin}
+                        className="flex"
+                      >
                         <FaLinkedin size={20} className="mr-2" />
                         Linkedin
                       </Link>
-                    </p>
-                    <p className="text-muted-foreground flex items-start text-sm mb-1">
-                      <Link href={company.ContactInfo?.twitter ? company.ContactInfo?.twitter : "#"} className="flex">
+                    )}
+                  </p>
+                  <p className="text-muted-foreground flex items-start text-sm mb-1">
+                    {company?.ContactInfo?.twitter && (
+                      <Link
+                        href={company?.ContactInfo?.twitter}
+                        className="flex"
+                      >
                         <FaXTwitter size={20} className="mr-2" />
                         Twitter
                       </Link>
-                    </p>
+                    )}
+                  </p>
                 </div>
               </CardContent>
             </Card>
@@ -130,41 +180,41 @@ const page = async ({params}) => {
               <CardContent className="grid gap-8">
                 <div className="flex items-center gap-4">
                   <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                        Name
-                    </p>
+                    <p className="text-sm font-medium leading-none">Name</p>
                     <p className="text-sm text-muted-foreground">
-                      {company.CEO.name}
+                      {company?.CEO?.name}
                     </p>
                   </div>
                 </div>
                 <div className="flex items-center gap-4">
                   <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                      Bio
-                    </p>
+                    <p className="text-sm font-medium leading-none">Bio</p>
                     <p className="text-sm text-muted-foreground">
-                    {company.CEO.bio}
+                      {company?.CEO?.bio}
                     </p>
                   </div>
                 </div>
                 <div className="flex items-center gap-4">
                   <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                      LinkedIn
-                    </p>
+                    <p className="text-sm font-medium leading-none">LinkedIn</p>
                     <p className="text-sm text-muted-foreground underline">
-                      <Link href={company.CEO.linkedin ? company.CEO.linkedin :"#"}>{company.CEO.linkedin}</Link>
+                      {company?.CEO?.linkedin && (
+                        <Link href={company?.CEO?.linkedin}>
+                          {company?.CEO?.linkedin}
+                        </Link>
+                      )}
                     </p>
                   </div>
                 </div>
                 <div className="flex items-center gap-4">
                   <div className="grid gap-1">
-                    <p className="text-sm font-medium leading-none">
-                      Twitter
-                    </p>
+                    <p className="text-sm font-medium leading-none">Twitter</p>
                     <p className="text-sm text-muted-foreground underline">
-                      <Link href={company.CEO.twitter ? company.CEO.twitter : "#"}>{company.CEO.twitter}</Link>
+                      {company?.CEO?.twitter && (
+                        <Link href={company?.CEO?.twitter}>
+                          {company?.CEO?.twitter}
+                        </Link>
+                      )}
                     </p>
                   </div>
                 </div>
