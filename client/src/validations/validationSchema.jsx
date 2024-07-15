805ae9e865c391603d1cf04b44a563103280d98a
schinak-m <shinak.x3@gmail.com>
2024-07-15 15:30:58 +0300

    build post job functionality

--- client/src/validations/validationSchema.jsx
+++ client/src/validations/validationSchema.jsx
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-export const validationSchema = Yup.object().shape({
+export const postJobSchema = Yup.object().shape({
   title: Yup.string().required("Job title is required"),
   jobType: Yup.string().required("Job Type is required"),
   workSetting: Yup.string().required("Work Setting is required"),
@@ -9,7 +9,6 @@ export const validationSchema = Yup.object().shape({
     .typeError("Wage must be a number")
     .required("Wage is required"),
   description: Yup.string().required("Job Description is required"),
-  name: Yup.string().required("Company Name is required"),
   location: Yup.string().required("Location is required"),
   deadline: Yup.date().required("Application Deadline is required"),
 });
